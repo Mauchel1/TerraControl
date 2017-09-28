2cda1c3c112c91026d16d8e1f82401172a6b348b
Mauchel1 <DanielDanielfriedrich@gmail.com>
2017-09-28 03:44:45 +0200

    Inhalt autopush hinzugefuegt

--- autopush.sh
+++ autopush.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+cd terra
+
+git add fuetterung.txt
+git add haeutungen.txt
+git add saeuberungen.txt
+git add schlangendaten.txt
+
+git commit -m "automatic commit - Update data"
+
+git push
